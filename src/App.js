927f54e36979d7c23a24854958faa72fc08e547e
Caio Lelis <caiolelis337@hotmail.com>
2022-03-14 19:35:13 -0300

    fechado

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Avaliacao from "./pages/avaliacao/avaliacao";
+
 import { Provider} from '@gitbook/unstated';
 import Administrador from './pages/administrador/administrador';
 import Login from './pages/login/login';
 import Registro from './pages/login/registro';
+import NotReady from './pages/avaliacao/notReady';
 
 function App(){
 
@@ -16,6 +18,7 @@ function App(){
         <Route exact path="/" component= {Login}/>
         <Route exact path="/administrador" component= {Administrador}/>
         <Route exact path="/login" component= {Login}/>
+        <Route exact path="/avaliacao" component= {NotReady}/>
         <Route exact path="/registro" component= {Registro}/>
       </Switch>
     </Router>
---
+++ src/pages/avaliacao/notReady.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotReady = () => {
+  return (
+    <div>A temporada de envio das avaliações ainda não começou, por favor aguarde a data de seu subsistema.</div>
+  )
+}
+
+export default NotReady
