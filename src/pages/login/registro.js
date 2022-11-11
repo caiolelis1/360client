45f569710cd685d7fa01f3141fd48a3e9d53adc9
caiolelis1 <36503087+caiolelis1@users.noreply.github.com>
2022-11-11 14:33:11 -0300

    Update registro.js

--- src/pages/login/registro.js
+++ src/pages/login/registro.js
@@ -1,67 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
-import { Grid, styled, Paper, TextField, Button, Select, InputLabel, MenuItem, Typography, Link, Avatar } from "@material-ui/core";
-import Alert from '@mui/material/Alert';
-import TeslaLogo from './tesla-logo.png';
-
+import { Grid, styled, Paper, TextField, Button, Select, InputLabel, MenuItem, Typography, Link, Avatar } from "@material-ui/core"
+import TeslaLogo from './logo_tesla1.jpg'
 const Registro = () => {
 
     const [subsistemas, setSubsistemas] = useState([]);
     const [subsistema, setSubsistema] = useState();
+    const [sistema, setSistema] = useState();
     const [usuario, setUsuario] = useState();
     const [nome, setNome] = useState();
-    const [presenha, setpreSenha] = useState('');
-    const [senha, setSenha] = useState('');
-    const [aux, setAux] = useState(0);
+    const [senha, setSenha] = useState();
 
-    let VerificaSenha = (props) => {
-        if (presenha === props) {
-            setSenha(props);
-        }
-    }
-    function Avisos_senha(props) {
-        if (presenha === props && props !== '' && aux < 2) {
-            return (
-                <Alert variant="outlined" severity="warning" onClose={(e) => { setAux(2) }}>
-                    Lembre-se de anotar a sua senha, ela é só sua, criptografada e inalterável.
-                </Alert>
-            )
-        }
-        if (props !== presenha && presenha !== '' && aux < 1) {
-            return (
-                <Alert variant="outlined" severity="error" onClose={(e) => { setAux(1) }}>
-                    As senhas devem coincidir.
-                </Alert>
-            )
-        }
-        else {
-            return ('')
-        }
-    }
 
     const registrar = () => {
+
         Axios.post("https://avaliacao-360.herokuapp.com/api/registrar", {
             usuario: usuario,
             subsistema: subsistema,
+            sistema: sistema,
             nome: nome,
             senha: senha,
-        }).then((response) => {
+        }).then((response)=>{
+
             console.log(response);
-            if (response.data !== '') {
-                alert("Sua conta foi criada com sucesso, " + nome + "!")
+
+            if(response.data!==''){
+                alert("SUA CONTA FOI CRIADA COM SUCESSO, "+nome+"! JÁ PODE AVALIAR.")
                 window.location.href = 'login';
             }
-            else {
-                alert("Por favor insira os dados corretamente.")
+            else{
+                alert("POR FAVOR INSIRA OS DADOS CORRETAMENTE")
             }
+
+            /*if(response.data.message){
+                setRegistroStatus(response.data.message)
+            }else{
+                setRegistroStatus("Sua conta foi criada com sucesso, "+ nomeRegistro +"! Já pode avaliar acima.");
+            }
+            console.log(response);*/
         })
+
     }
 
     const buscarSubsistemas = () => {
         Axios.post('https://avaliacao-360.herokuapp.com/api/selecionaSubsistemas').then((response) => {
-            if (response.data.message) {
+            if(response.data.message){
                 console.log(response.data.message)
-            } else {
+            } else{
                 setSubsistemas(response.data)
             }
         })
@@ -69,42 +54,48 @@ const Registro = () => {
 
     useEffect(() => {
         buscarSubsistemas();
-    }, [])
+    },[])
 
+    const paperStyle={padding: 20, height:'70vh', width:280, margin:"20px auto"}
+    const btstilo={margin:'10px 0'}
     const Bootbot = styled(Button)({
-        fontWeight: 600,
         backgroundColor: '#4ed840',
         borderColor: '#4ed840',
     })
-    const paperStyle = { padding: 20, height: '75vh', width: 280, margin: "20px auto" }
-    const btstilo = { margin: '10px 0', marginBottom: '20px' }
-    return (
+    return(
         <Grid>
-            <Paper elevation={10} style={paperStyle}>
-                <Grid align="center">
-                    <Avatar alt='rcc' src={TeslaLogo} />
-                    <h2>Cadastro</h2>
-                </Grid>
-                <InputLabel>Subsistema</InputLabel>
-                <Select label="Subsitema" fullWidth required onChange={(e) => { setSubsistema(e.target.value) }}>
-                    <MenuItem active value={null}>Escolha seu subsistema</MenuItem>
-                    {subsistemas.map((subsistema) => (
-                        <MenuItem key={subsistema.idsubsistema} value={subsistema.idsubsistema}>{subsistema.nome}</MenuItem>
-                    ))}
-                </Select>
-                <TextField label="Nome de Usuário" placeholder="Defina o User" fullWidth required onChange={(e) => { setUsuario(e.target.value) }} />
-                <TextField label="Nome Completo" placeholder="Insira seu Nome" fullWidth required onChange={(e) => { setNome(e.target.value) }} />
-                <TextField label="Senha" placeholder="Insira a Senha" type='password' fullWidth required onChange={(e) => { setpreSenha(e.target.value) }} />
-                <TextField label="Confirma Senha" placeholder="Confirme a Senha" type='password' fullWidth required onChange={(e) => { VerificaSenha(e.target.value) }} />
-                {Avisos_senha(senha)}
-                <Bootbot type="submit" variant='contained' onClick={() => registrar()} fullWidth style={btstilo}> Cadastrar</Bootbot>
-                <Typography>
-                    Já possui cadastro? <Link href='login'> Voltar ao Login</Link>
-                </Typography>
-            </Paper>
+                <Paper elevation={10} style={paperStyle}>
+                    <Grid align="center">
+                    <Avatar alt='rcc' src={TeslaLogo}/>
+                        <h2>Cadastro</h2>
+                    </Grid>
+                    <InputLabel>Sistemas</InputLabel>
+                        <Select label="Sistema" fullWidth required onChange={(e)=>{setSistema(e.target.value)}}>
+                            <MenuItem active value={null}>Escolha seu sistema</MenuItem>
+                            <MenuItem value='1'>Administração</MenuItem>
+                            <MenuItem value='2'>Elétrica</MenuItem>
+                            <MenuItem value='3'>Mecânica</MenuItem>
+                        </Select>
+                    <InputLabel>Subsistema</InputLabel>
+                        <Select label="Subsistema" fullWidth required onChange={(e)=>{setSubsistema(e.target.value)}}>
+                            <MenuItem active value={null}>Escolha seu subsistema</MenuItem>
+                            {subsistemas.map((subsistema) =>(
+                                <MenuItem key={subsistema.idsubsistema} value={subsistema.idsubsistema}>{subsistema.nome}</MenuItem>
+                            ))}
+                        </Select>
+                    <TextField label="Nome de Usuário" placeholder="Defina o User" fullWidth required onChange={(e)=>{setUsuario(e.target.value)}}/>
+                    <TextField label="Nome Completo" placeholder="Insira seu Nome" fullWidth required onChange={(e)=>{setNome(e.target.value)}}/>
+                    <TextField label="Senha" placeholder="Insira a Senha" type='password' fullWidth required onChange={(e)=>{setSenha(e.target.value)}}/>
+                    <Bootbot type="submit" variant='contained' onClick={() => registrar()} fullWidth style={btstilo}> Cadastrar</Bootbot>
+                    <Typography> Já possui cadastro?
+                        <Link href='login'>
+                            Voltar ao Login
+                        </Link>
+                    </Typography>
+                </Paper>
         </Grid>
     )
-
+  
 };
 
 export default Registro;
