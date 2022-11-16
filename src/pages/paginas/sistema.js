4d002ef83fffa904acadab1a1119bbd6a4ca9356
caiolelis1 <36503087+caiolelis1@users.noreply.github.com>
2022-11-16 15:13:06 -0300

    Update sistema.js

--- src/pages/paginas/sistema.js
+++ src/pages/paginas/sistema.js
@@ -2,6 +2,14 @@ import React from "react";
 
 function Sistema (){
 
+    //buscar sistema no db
+
+    //buscar notas de cada membro no db
+
+    //fazer media total e de cada membro
+    //mostrar cada nota separadamente
+    //fazer grafico?
+
     return(
         <div>Sistema</div>
     )
