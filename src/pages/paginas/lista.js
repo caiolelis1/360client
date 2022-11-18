4a0bc5816120cc46b766156f76137f3a3ba05c6d
caiolelis1 <36503087+caiolelis1@users.noreply.github.com>
2022-11-18 10:58:37 -0300

    Add files via upload

---
+++ src/pages/paginas/lista.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Lista (){
+
+    //buscar sistema no db
+
+    //buscar notas de cada membro no db
+
+    //fazer media total e de cada membro
+    //mostrar cada nota separadamente
+    //fazer grafico?
+
+    return(
+        <div>Lista</div>
+    )
+}
+
+export default Lista;
