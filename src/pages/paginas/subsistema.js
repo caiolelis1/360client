cf8812ad0100ab37c89333907cf0c47276332b50
marcos lott <84547699+lottmarcos@users.noreply.github.com>
2022-11-22 12:11:43 -0300

    Update subsistema.js

--- src/pages/paginas/subsistema.js
+++ src/pages/paginas/subsistema.js
@@ -1,68 +1,174 @@
 import React, { useEffect, useState } from "react";
+import { Grid, styled, Paper, Button, Container, Typography, Link } from "@material-ui/core"
 import Axios from 'axios';
 import { useParams } from "react-router-dom";
 
-function Subsistema (){
+function Subsistema() {
+   const [subsistema, setSubsistema] = useState({});
+   const [users, setUsers] = useState([]);
+   const [notas, setNotas] = useState([]);
+   const [blocos, setBlocos] = useState([]);
 
-    //buscar sistema no db
+   const containerStyle = { padding: 20, margin: "20px auto", maxWidth: 1200 };
+   const btstilo2 = { margin: '10px 10px', width: 230 };
+   const Bootbot = styled(Button)({
+      backgroundColor: '#4ed840',
+      borderColor: '#4ed840',
+   });
 
-    //buscar notas de cada membro no db
+   const params = useParams();
+   let id = params.id;
 
-    //fazer media total e de cada membro
-    //mostrar cada nota separadamente
-    //fazer grafico?
+   const buscarSubsistema = (id) => {
+      Axios.post('https://avaliacao-360.herokuapp.com/api/selecionaSubsistema', {
+         id: id,
+      }).then((response) => {
+         console.log(response.data[0])
+         setSubsistema(response.data[0])
+      })
+   }
 
-    const [subsistema, setSubsistema] = useState({});
-    const [users, setUsers] = useState([]);
-    const [notas, setNotas] = useState([]);
+   const buscarUsers = (id) => {
+      Axios.post('https://avaliacao-360.herokuapp.com/api/buscarUsersSubsistema', {
+         subsistemaid: id,
+      }).then((response) => {
+         console.log(response)
+         setUsers(response.data)
+      })
+   }
 
-    const params = useParams();
-    let id = params.id;
+   const buscarNotas = (id) => {
+      Axios.post('https://avaliacao-360.herokuapp.com/api/selecionaAvaliacoesPessoa', {
+         id: id,
+      }).then((response) => {
+         setNotas(response.data);
+         console.log(response.data)
+      })
+   }
 
-    const buscarSubsistema = (id) => {
-        Axios.post('https://avaliacao-360.herokuapp.com/api/selecionaSubsistema', {
-            id: id,
-        }).then((response) => {
-            console.log(response.data[0])
-            setSubsistema(response.data[0])
-        })
-    }
+   useEffect(() => {
+      buscarSubsistema(id);
+      buscarUsers(id);
+   }, [id]);
 
-    const buscarUsers = (id) => {
-        Axios.post('https://avaliacao-360.herokuapp.com/api/buscarUsersSubsistema', {
-            subsistemaid: id,
-        }).then((response) =>{
-            console.log(response)
-            setUsers(response.data)
-        })
-    }
+   useEffect(() => {
+      console.log(users)
+      for (let i = 0; i < users.length; i++) {
+         buscarNotas(users[i].idpessoa)
+      }
+   }, [users])
 
-    const buscarNotas = (id) => {
-        Axios.post('https://avaliacao-360.herokuapp.com/api/selecionaAvaliacoesPessoa', {
-           id: id,
-        }).then((response) => {
-           setNotas(response.data);
-           console.log(response.data)
-        })
-     }
+   function media(tipo) {
 
-    useEffect(() => {
-        buscarSubsistema(id);
-        buscarUsers(id);
-    }, [id]);
+      const result = notas.filter(item => item.referenciaidtipoavaliacao === tipo);
+      let resultLength = 0;
+      let media = 0;
+      for (let i in result) {
+         console.log(result[i].nota)
 
-    useEffect(() => {
-        console.log(users)
-        for (let i = 0; i < users.length; i++){
-            buscarNotas(users[i].idpessoa)
-        }
-    }, [users])
+         if (result[i].nota) {
 
-    return(
-        <div>
-            <h2>{subsistema.nome}</h2>
-        </div>
-    )
+            resultLength++;
+
+            result[i].nota = parseInt(result[i].nota, 10);
+
+            media += result[i].nota;
+         }
+
+      }
+
+      media = media / resultLength;
+      const mediaArredondada = +(media.toFixed(1))
+
+      return mediaArredondada;
+
+   }
+
+   function imprimeNotas(title, id) {
+      return (
+         <form>
+            <p className="tituloTipoAvaliacao">{title}</p>
+
+            <p>Media:</p>
+            <p>
+               {media(id)}
+            </p>
+
+         </form>
+      )
+   }
+
+   function imprimeBlocos() {
+      console.log("imprimindo blocos")
+      const paperStyle = { padding: 20, width: 600, margin: "20px auto" }
+      const blockStyle = { color: "white", backgroundColor: "white", margin: "auto" }
+      const gridStyle = { padding: 5 }
+      const aux = [];
+      let children = [];
+
+      children.push((
+         <div className="blocoPessoa" style={blockStyle}>
+            <Grid style={gridStyle}>
+               <Paper elevation={10} style={paperStyle}>
+                  <p className="tituloNomePessoa">{subsistema.nome}</p>
+                  {imprimeNotas("Comunicação", 1)}
+                  {imprimeNotas("Companheirismo", 2)}
+                  {imprimeNotas("Pontualidade", 3)}
+                  {imprimeNotas("Proatividade", 4)}
+                  {imprimeNotas("Engajamento / Motivação", 5)}
+                  {imprimeNotas("Trabalho em Equipe", 6)}
+                  {imprimeNotas("Presença", 24)}
+                  {imprimeNotas("Participação", 34)}
+                  {imprimeNotas("Cumprimento de tarefas", 44)}
+                  {imprimeNotas("Comprometimento", 54)}
+                  {imprimeNotas("Organização", 64)}
+                  {imprimeNotas("Imparcialidade", 74)}
+                  {imprimeNotas("Facilitador", 84)}
+                  {imprimeNotas("Dar Autonomia", 94)}
+                  {imprimeNotas("Acessibilidade", 104)}
+                  {imprimeNotas("Uso da Suati", 144)}
+               </Paper>
+
+            </Grid>
+         </div>
+
+      ));
+
+      aux.push((
+         <React.Fragment key={subsistema.id}>
+            {children}
+         </React.Fragment>
+      ));
+
+      setBlocos(aux);
+   }
+
+   return (
+      <div>
+         <Grid>
+            <div className="pageAvaliacao">
+               <p></p>
+               <img src="https://static.wixstatic.com/media/d979eb_5158f599534e4f0480b44a267fc68e83~mv2_d_3508_2480_s_4_2.png/v1/fill/w_367,h_259,al_c,q_85,usm_0.66_1.00_0.01/d979eb_5158f599534e4f0480b44a267fc68e83~mv2_d_3508_2480_s_4_2.webp"
+                  width="100" height="70" />
+
+               <h3>Avaliação 360 | Versão Web 0.1</h3>
+               <Container elevation={5} style={containerStyle}>
+
+                  <Bootbot type="submit" variant='contained' onClick={() => imprimeBlocos()} fullWidth style={btstilo2}> Ver Resultados</Bootbot>
+                  <Typography>
+                     <Link href='../lista'>
+                        Voltar Para a Lista.
+                     </Link>
+                  </Typography>
+                  <div>
+                     {blocos}
+                  </div>
+
+               </Container>
+            </div>
+         </Grid>
+      </div>
+   )
 }
 
 export default Subsistema;
