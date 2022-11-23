7900be2d27a9d2660e6ed55bc5f179c24d27d145
caiolelis1 <36503087+caiolelis1@users.noreply.github.com>
2022-11-23 13:15:20 -0300

    Update membro.js

--- src/pages/paginas/membro.js
+++ src/pages/paginas/membro.js
@@ -27,7 +27,7 @@ function Membro() {
             userid: id2,
         }).then((response) => {
             console.log(response.data[0].idsistema + " " + user.idsistema)
-            if(!( ( response.data[0].idpessoa==id ) || ( response.data[0].diretor==1 && response.data[0].referenciaidsubsistema==user.referenciaidsubsistema ) || ( response.data[0].diretorGeral==1 && response.data[0].sistemaID==user.sistemaID ) || ( response.data[0].capitao==1 ) ) ){
+            if(!( ( response.data[0].idpessoa==id ) || ( response.data[0].diretor==1 && response.data[0].referenciaidsubsistema==user.referenciaidsubsistema ) || ( response.data[0].diretorGeral==1 && response.data[0].sistemaID==user.sistemaID ) || ( response.data[0].capitao==1 ) || ( response.data[0].admin==1 ) ) ){
                 console.log('PRECISA SER REDIRECIONADO')
             } else{
                console.log('PODE FICAR')
